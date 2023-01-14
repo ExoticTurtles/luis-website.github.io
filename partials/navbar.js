e52e5c71299b651a969e27e5cd9424336c2b45fc
ExoticTurtles <daniel.garciaea@uanl.edu.mx>
2023-01-14 14:04:38 -0600

    Some other changes

--- partials/navbar.js
+++ partials/navbar.js
@@ -1,15 +1,28 @@
 const headerTemplate = document.createElement('template');
 headerTemplate.innerHTML = `
-<header class="mb-auto">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
+
+<link rel="stylesheet" href="./public/stylesheets/stylesheet.css">
+
+<header class="mb-auto bg-dark">
 <div>
-<h3 class="float-md-start mb-0">Luis Daniel García</h3>
+
+<h4 class="float-md-start mb-0">
+<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-brush-fill" viewBox="0 0 16 16">
+<path d="M15.825.12a.5.5 0 0 1 .132.584c-1.53 3.43-4.743 8.17-7.095 10.64a6.067 6.067 0 0 1-2.373 1.534c-.018.227-.06.538-.16.868-.201.659-.667 1.479-1.708 1.74a8.118 8.118 0 0 1-3.078.132 3.659 3.659 0 0 1-.562-.135 1.382 1.382 0 0 1-.466-.247.714.714 0 0 1-.204-.288.622.622 0 0 1 .004-.443c.095-.245.316-.38.461-.452.394-.197.625-.453.867-.826.095-.144.184-.297.287-.472l.117-.198c.151-.255.326-.54.546-.848.528-.739 1.201-.925 1.746-.896.126.007.243.025.348.048.062-.172.142-.38.238-.608.261-.619.658-1.419 1.187-2.069 2.176-2.67 6.18-6.206 9.117-8.104a.5.5 0 0 1 .596.04z"/>
+</svg>
+Luis Daniel García Leal</h4>
 <nav class="nav nav-masthead justify-content-center float-md-end">
     <a href="#" class="nav-link active" aria-current="page">Home</a>
-    <a class="nav-link" href="/views/habilidades">Habilidades</a>
+    <a class="nav-link" href="/views/skills">Skills</a>
 </nav>
 </div>
 </header>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 `
+
 
 class Header extends HTMLElement {
     constructor() {
