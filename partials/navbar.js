87f7bcac9dd2ab307761b5310f305f9a30f2f21d
ExoticTurtles <daniel.garciaea@uanl.edu.mx>
2023-01-30 14:11:43 -0600

    Cambios menores

--- partials/navbar.js
+++ partials/navbar.js
@@ -6,18 +6,21 @@ headerTemplate.innerHTML = `
 <link rel="stylesheet" href="./public/stylesheets/stylesheet.css">
 
 <header class="mb-auto">
-<div class="p-3">
-
-<h4 class="float-md-start mb-0">
-<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-brush-fill" viewBox="0 0 16 16">
-<path d="M15.825.12a.5.5 0 0 1 .132.584c-1.53 3.43-4.743 8.17-7.095 10.64a6.067 6.067 0 0 1-2.373 1.534c-.018.227-.06.538-.16.868-.201.659-.667 1.479-1.708 1.74a8.118 8.118 0 0 1-3.078.132 3.659 3.659 0 0 1-.562-.135 1.382 1.382 0 0 1-.466-.247.714.714 0 0 1-.204-.288.622.622 0 0 1 .004-.443c.095-.245.316-.38.461-.452.394-.197.625-.453.867-.826.095-.144.184-.297.287-.472l.117-.198c.151-.255.326-.54.546-.848.528-.739 1.201-.925 1.746-.896.126.007.243.025.348.048.062-.172.142-.38.238-.608.261-.619.658-1.419 1.187-2.069 2.176-2.67 6.18-6.206 9.117-8.104a.5.5 0 0 1 .596.04z"/>
-</svg>
-Luis Daniel García Leal</h4>
-<nav class="nav nav-masthead justify-content-center float-md-end">
-    <a href="#" class="nav-link active" aria-current="page">Home</a>
-    <a class="nav-link" href="/views/skills">Skills</a>
-</nav>
-</div>
+<nav class="navbar sticky-top navbar-expand-lg navbar-dark bg-dark">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Luis Daniel García Leal</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav ms-auto">
+        <a class="nav-link active" href="/">Home</a>
+        <a class="nav-link" href="/campgrounds">Skills</a>
+        <a class="nav-link" href="/campgrounds/new">Languages</a>
+        </div>
+      </div>
+    </div>
+  </nav>
 </header>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
