66bd77a1074020e093f6077f54e99ca7cd1f6e41
ExoticTurtles <daniel.garciaea@uanl.edu.mx>
2023-06-07 17:23:16 -0600

    Almost decent

--- partials/navbar.js
+++ partials/navbar.js
@@ -1,29 +1,44 @@
 const headerTemplate = document.createElement('template');
 headerTemplate.innerHTML = `
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
-
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
 <link rel="stylesheet" href="./public/stylesheets/stylesheet.css">
 
 <header class="mb-auto">
-<nav class="navbar sticky-top navbar-expand-lg">
-    <div class="container-fluid">
-      <a class="navbar-brand" href="#">Luis Daniel García Leal</a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-        <div class="navbar-nav ms-auto">
-        <a class="nav-link active" href="/">Home</a>
-        <a class="nav-link" href="/campgrounds">Skills</a>
-        <a class="nav-link" href="/campgrounds/new">Languages</a>
-        </div>
-      </div>
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <img src="public/icons/turtle.png" alt="Logo" width="30" height="24" class="d-inline-block align-text-top">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#certifications">Certifications</a>
+        </li>
+        <li class="nav-item">
+        <a class="nav-link" href="#bachelorCareer">Bachelor career</a>
+        </li>
+        <li class="nav-item">
+        <a class="nav-link" href="#portfolio">Portfolio</a>
+        </li>
+        <li class="nav-item">
+        <a class="nav-link" href="#personalInformation">Personal information</a>
+        </li>
+        <li class="nav-item">
+        <a class="nav-link" href="#workExperience">Work experience</a>
+        </li>
+
+      </ul>
     </div>
-  </nav>
+  </div>
+</nav>
 </header>
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
 `
 
 
